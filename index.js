002e02b434bb6585c8048869efccd2b986f7f966
Aisha Hussein <aishasalaam07@gmail.com>
2023-03-23 01:45:52 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+
+let main = document.getElementById('main')
+main.remove();
+
+// newHeader = document.create()
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.innerHTML = "YOUR-NAME is the champion"
